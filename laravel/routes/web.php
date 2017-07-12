3f455f1859d7a400503e51741757f8f348431514
Anand Singh <anand.singh989@gmail.com>
2017-07-12 16:33:09 +0530

    work done on laravel route correction and introduction of wireframe concept

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -10,34 +10,45 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
     return view('frontend.pages.index');
-});
-Route::get('/community',function(){
-	return view('frontend.pages.community-changed');
-});
-Route::get('/offer',function(){
-	return view('frontend.pages.offer-section');
-});
-Route::get('/loggedin',function(){
-	return view('frontend.pages.loggedin');
-});
-Route::get('/profile',function(){
-	return view('frontend.pages.profile');
-});
-Route::get('/shared-location',function(){
-	return view('frontend.pages.shared-location');
-});
-Route::get('/my-favourite',function(){
-	return view('frontend.pages.myfavourite');
-});
-Route::get('/moreevent',function(){
-	return view('frontend.pages.moreevent');
-});
-Route::get('/shared-location-more',function(){
-	return view('frontend.pages.shared-location-more');
-});
-Route::get('/shared-location-new',function(){
-	return view('frontend.pages.shared-location-new');
+})->name('fronted_home');
+
+Route::group(['prefix' => 'wireframe'], function() {
+	Route::get('/community',function(){
+		return view('frontend.pages.community-changed');
+	})->name('frontend_community_page');
+
+	Route::get('/offer',function(){
+		return view('frontend.pages.offer-section');
+	})->name('frontend_offer_page');
+
+	Route::get('/loggedin',function(){
+		return view('frontend.pages.loggedin');
+	})->name('frontent_logged_in');
+
+	Route::get('/profile',function(){
+		return view('frontend.pages.profile');
+	})->name('frontend_profile_page');
+
+	Route::get('/shared-location',function(){
+		return view('frontend.pages.shared-location');
+	})->name('frontend_shared_location');
+
+	Route::get('/my-favourite',function(){
+		return view('frontend.pages.myfavourite');
+	})->name('frontend_my_faourite');
+
+	Route::get('/moreevent',function(){
+		return view('frontend.pages.moreevent');
+	})->name('frontend_more_event');
+
+	Route::get('/shared-location-more',function(){
+		return view('frontend.pages.shared-location-more');
+	})->name('frontend_shared_location_more');
+
+	Route::get('/shared-location-new',function(){
+		return view('frontend.pages.shared-location-new');
+	})->name('frontend_shared_location_new');
+
 });
