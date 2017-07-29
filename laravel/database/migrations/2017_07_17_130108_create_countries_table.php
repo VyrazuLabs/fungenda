a26b4cdf3af3631e5f142da596192dc29b8970dc
Anand Singh <anand.singh989@gmail.com>
2017-07-29 16:22:01 +0530

    work for migration issues fix

--- laravel/database/migrations/2017_07_17_130108_create_countries_table.php
+++ laravel/database/migrations/2017_07_17_130108_create_countries_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCountryTable extends Migration
+class CreateCountriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -28,6 +28,6 @@ class CreateCountryTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('country');
+        Schema::dropIfExists('countries');
     }
 }
