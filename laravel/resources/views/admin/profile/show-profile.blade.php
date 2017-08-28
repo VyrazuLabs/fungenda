8f70b30b2baa7e964a947d605acf311513357742
Anand <anand.singh989@gmail.com>
2017-08-28 12:01:58 +0530

    working on admin profile section

--- laravel/resources/views/admin/profile/show-profile.blade.php
+++ laravel/resources/views/admin/profile/show-profile.blade.php
@@ -123,19 +123,6 @@
 
 <!-- ./wrapper -->
 @section('add-js')
-	<!-- jQuery 3 -->
-<script src="{{url('/bower_components/jquery/dist/jquery.min.js')}}"></script>
-<!-- Bootstrap 3.3.7 -->
-<script src="{{url('/bower_components/bootstrap/dist/js/bootstrap.min.js')}}"></script>
-<!-- FastClick -->
-<script src="{{url('/bower_components/fastclick/lib/fastclick.js')}}"></script>
-<!-- AdminLTE App -->
-<script src="{{url('/dist/js/adminlte.min.js')}}"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="{{url('/dist/js/demo.js')}}"></script>
+
 @endsection
 
-
-
-</body>
-</html>
