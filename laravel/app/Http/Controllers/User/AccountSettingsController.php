ef3b927ec5413c1da5159cb81afc29193d93b20a
Anand <anand.singh989@gmail.com>
2017-09-06 15:32:08 +0530

    work done on email settings section

--- laravel/app/Http/Controllers/User/AccountSettingsController.php
+++ laravel/app/Http/Controllers/User/AccountSettingsController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use Auth;
 use Session;
 use App\Models\User;
+use App\Models\EmailNotificationSettings;
 use Validator;
 
 class AccountSettingsController extends Controller
@@ -14,7 +15,10 @@ class AccountSettingsController extends Controller
 	
 	//Return view of account settings
 	public function view(){
-		return view('frontend.pages.accountsetting');
+
+		$data['email_notification'] = EmailNotificationSettings::where('user_id',Auth::user()->user_id)->first();
+		
+		return view('frontend.pages.accountsetting',$data);
 	}
 
 	//Function for password update
@@ -47,6 +51,41 @@ class AccountSettingsController extends Controller
 	    		Session::flash('error', "Your current password is wrong");
 	    			return redirect()->back();
 	    	}
+    	}
+    }
+
+    // Save notification settings
+    public function saveNotificationSettings(Request $request){
+
+    	$input = $request->input();
+
+    	if(array_key_exists('notification_enabled',$input) == 1){
+    		$notification_enabled = 1;
+    	}
+    	else{
+    		$notification_enabled = 0;
+    	}
+
+    	$data = EmailNotificationSettings::where('user_id',Auth::user()->user_id)->first();
+
+    	if(empty($data)){
+	    	EmailNotificationSettings::create([
+	    			'user_id' => Auth::user()->user_id,
+	    			'notification_enabled' => $notification_enabled,
+	    			'notification_frequency' => $input['notification_frequency']
+	    		]);
+
+	    	Session::flash('success', "Email notification settings successfully updated");
+	    	return redirect()->back();
+    	}
+    	else{
+    		$data->update([
+    				'notification_enabled' => $notification_enabled,
+	    			'notification_frequency' => $input['notification_frequency']
+    			]);
+
+    		Session::flash('success', "Email notification settings successfully updated");
+	    	return redirect()->back();
     	}
     }
 
---
+++ laravel/database/migrations/2017_09_06_092135_create_email_notification_setting.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmailNotificationSetting extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('email_notification_setting', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('user_id');
+            $table->integer('notification_enabled');
+            $table->integer('notification_frequency');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('email_notification_setting');
+    }
+}
