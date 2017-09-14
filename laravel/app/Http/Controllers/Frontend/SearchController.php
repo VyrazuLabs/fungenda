6d87b75c676d92cc44e0c2e94e4ca2e750868516
Anand <anand.singh989@gmail.com>
2017-09-14 20:47:08 +0530

    working on search functionality

--- laravel/app/Http/Controllers/Frontend/SearchController.php
+++ laravel/app/Http/Controllers/Frontend/SearchController.php
@@ -7,21 +7,84 @@ use App\Http\Controllers\Controller;
 use App\Models\Event;
 use App\Models\Business;
 use App\Models\Address;
+use App\Models\Tag;
+use App\Models\AssociateTag;
+use App\Models\Category;
 
 class SearchController extends Controller
 {
     public function search(Request $request){
     	$input = $request->input();
-        echo "<pre>";print_r($input);die;
+
     	if($input['radio'] == 1){
-    		// echo "business";
-    		
-    		$search_by_location = Business::where('business_venue','like','%'.$input['location'].'%')->get();
-    		echo "<pre>";
-    		print_r($search_by_location);
+
+            if(isset($input['location'])){
+                $location_array = explode(',',$input['location']);
+                    foreach ($location_array as $location) {
+                        $search_by_location[] = Business::where('business_venue','like','%'.$location.'%')->get();
+                        $all_search_business =  $search_by_location;
+                    }
+                // echo "<pre>";
+                // print_r($all_search_business);die;
+                foreach ($all_search_business as $business_search) {
+                    foreach ($business_search as $business) {
+                        $business_count = count($business->getFavorite()->where('status',1)->get());
+                        $business['fav_count'] = $business_count;
+                        $img = explode(',',$business['business_image']);
+                        $business['image'] = $img;
+                        $related_tags = $business->getTags()->where('entity_type',1)->get();
+                        $business['tags'] = $related_tags;
+                     }       
+                }
+            }
+            if(isset($input['tags'])){
+
+                $tag_id_all = [];
+                $tag_details_all = [];
+                $all_user_id = [];
+                $all_tag_user_details = [];
+                $final_tags_array = [];
+                $final_unique_value_tags_array_business_id = [];
+                $all_search_tags = [];
+
+                foreach ($input['tags'] as $tag) {
+                    $tag_details = Tag::where('tag_name',$tag)->first();
+                    if(!empty($tag_details)){
+                        $tag_id = $tag_details->tag_id;  
+                        $tag_id_all[] = $tag_id;
+                    } 
+                }
+                foreach ($tag_id_all as $tag_id) {
+                    $tag_user_details = AssociateTag::where('tags_id','like','%'.$tag_id.'%')->where('entity_type',1)->get();
+                    $all_tag_user_details[] = $tag_user_details;
+                }
+                foreach ($all_tag_user_details as $single_tag_user_details) {
+                    foreach ($single_tag_user_details as $key => $value) {
+                        $final_tags_array[] = $value;
+                    }
+                }
+                $final_unique_value_tags_array = array_unique($final_tags_array);
+                foreach ($final_unique_value_tags_array as $final_unique_value_tags_user_id) {
+                    $final_unique_value_tags_array_business_id[] = $final_unique_value_tags_user_id['entity_id'];
+                }
+                foreach ($final_unique_value_tags_array_business_id as $business_id) {
+                    $search_by_tag = Business::where('business_id',$business_id)->first();
+                    $all_search_business[] = $search_by_tag;
+                }
+            }
+
+            $all_category = Category::where('parent',0)->get();
+                foreach ($all_category as $category) {
+                        $category['sub_category'] = Category::where('parent',$category['category_id'])->pluck('name','category_id');
+                    }
+            return view('frontend.pages.index',compact('all_search_business','all_category'));
     	}
     	else{
-    		echo "event";
-    	}
+    		$location_array = explode(',',$input['location']);
+                foreach ($location_array as $location) {
+                    $search_by_location = Event::where('event_venue','like','%'.$location.'%')->get();
+                    $all_search_location[] =  $search_by_location;
+    	   }
+        }
     }
 }
--- laravel/resources/views/frontend/pages/index.blade.php
+++ laravel/resources/views/frontend/pages/index.blade.php
@@ -10,6 +10,7 @@
 	<div class="container">
 		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 query-div">
 		{{ Form::open(['method'=>'post','files'=>'true','url'=>'/search']) }}
+			<div id="map"></div>
 			<div class="cl-lg-12 col-md-12 col-xs-12 radio-btn">
 				<label class="custom-control custom-radio">
 	  				<input value="1" id="radio1" name="radio" type="radio" class="custom-control-input">
@@ -45,13 +46,13 @@
 		      				</select>
 		      			</div>
 					</div>
-					<div class="form-group indexformdiv">
+					<div class="form-group indexformdiv" id="fromDateDiv">
 						<label for="FromDate">From Date</label>
 	      				<input type="text" id="fromdate" name="fromdate" class="form-control boxinput datecalender datecalen" placeholder="Select From Date">
 	      				<i class="fa fa-calendar hometime" aria-hidden="true"></i>
 	    				{{-- <span class="glyphicon glyphicon-calendar hometime"></span> --}}
 	    			</div>
-					<div class="form-group indexformdiv">
+					<div class="form-group indexformdiv" id="toDateDiv">
 						<label for="ToDate">To Date</label>
       					<input type="text" id="todate" name="todate" class="form-control boxinput datecalender" placeholder="Select To Date">
       					<i class="fa fa-calendar hometime" aria-hidden="true"></i>
@@ -89,10 +90,11 @@
 				<div class="col-lg-9 col-md-9 col-sm-12 col-xs-12 left-div">
 					<div class="col-lg-12 col-md-12 col-xs-12 leftcardshadow">
 						<div class="customdetail">
-							@if( count($all_business) > 0 )
+
+							@if(isset($all_search_business))
 								<div class="businessmain businessevent">
 									<h3 class="business-text">Businesses:</h3>
-									@foreach($all_business as $business)
+									@foreach($all_search_business[0] as $business)
 									<div class="col-lg-12 col-md-12 col-xs-12 devide">
 										<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 divimgs">
 											<a href="{{ route('frontend_more_business',['q'=>$business['business_id']]) }}">
@@ -109,7 +111,7 @@
 											</a>
 										</div>
 										<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 textdetails">
-											<h4 class="head"><a href="{{ route('frontend_more_business',['q'=>$business['business_id']]) }}">{{ $business['business_title'] }}</a></h4>
+											<h4 class="head"><a href="{{ route('frontend_more_business',['q'=>$business[0]['business_id']]) }}">{{ $business['business_title'] }}</a></h4>
 											
 										@if( count($business['tags']) > 0 )
 											<h5 class="colors">Listed in 
@@ -167,31 +169,32 @@
 										
 									</div>
 								</div>
-							@endif
-							<!--end business div-->
-							<!--start event div-->
-							@if( count($all_events) > 0 )
-								<div class="eventmain businessevent">
-									<h3 class="business-text">Events:</h3>
-									@foreach($all_events as $event)
-									<div class="col-lg-12 col-md-12 col-xs-12 devide">
-										<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 divimgs">
-											<a href="{{ route('frontend_more_event',['q'=>$event['event_id']]) }}">
+							@else
+								@if( count($all_business) > 0 )
+									<div class="businessmain businessevent">
+										<h3 class="business-text">Businesses:</h3>
+										@foreach($all_business as $business)
+										<div class="col-lg-12 col-md-12 col-xs-12 devide">
+											<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 divimgs">
+												<a href="{{ route('frontend_more_business',['q'=>$business['business_id']]) }}">
 
-												@if(file_exists(public_path().'/'.'images'.'/'.'event/'.$event['image'][0]) == 1)
-													<img src="{{ url('/images/event/'.$event['image'][0]) }}" class="img-responsive thumb-img placeholder">
-												@else
-													<img src="{{ url('/images/placeholder.svg') }}" class="img-responsive thumb-img placeholder">
+													@if(file_exists(public_path().'/'.'images'.'/'.'business/'.$business['image'][0]) == 1)
 
-												@endif
-											</a>
-										</div>
-										<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 textdetails">
-											<h4 class="head"><a href="{{ route('frontend_more_event',['q'=>$event['event_id']]) }}">{{ $event['event_title'] }}</a></h4>
+														<img src="{{ url('images/business/'.$business['image'][0]) }}" class="img-responsive thumb-img placeholder">
 
-											@if( count($event['tags']) > 0 )
+													@else
+
+														<img src="{{ url('images/placeholder.svg') }}" class="img-responsive thumb-img placeholder">
+
+													@endif
+												</a>
+											</div>
+											<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 textdetails">
+												<h4 class="head"><a href="{{ route('frontend_more_business',['q'=>$business['business_id']]) }}">{{ $business['business_title'] }}</a></h4>
+												
+											@if( count($business['tags']) > 0 )
 												<h5 class="colors">Listed in 
-												@foreach($event['tags'] as $value)
+												@foreach($business['tags'] as $value)
 													@php
 														$unserialize_array = unserialize($value['tags_id']);
 													@endphp
@@ -202,57 +205,136 @@
 												</h5>
 											@endif
 
-											<p class="left-sub-text">Finger foods including burgers. This bar is sort of perfect.First of all it's right across from the police station...</p>
-											<p class="read">
-												<a href="{{ route('frontend_more_event',['q'=>$event['event_id']]) }}">Read More </a>
-												<a target="_blank" href="{{ $event['event_website'] }}">| Website</a>
-												@if(Auth::check() && Auth::user()->user_id == $event->created_by)
-													<a href="{{ route('edit_event',['q'=> $event['event_id']]) }}">| Edit</a>
-												@endif
-											</p>
-										</div>
-										<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 text-center socialicon">
-											<div class="fav-btn-container">
-												@if(!Favourite::check($event['event_id'], 2))
-													<button type="button" data-id="{{ $event['event_id'] }}" class="btn favourite add_fav_event"><i class="fa fa-heart" aria-hidden="true"><span class="favourite-btn"> Add To Favourites</span></i></button>
-												@else
-													<button type="button"  data-id="{{ $event['event_id'] }}" class="btn favourite rvm_fav_event"><i class="fa fa-heart" aria-hidden="true"><span class="favourite-btn"> Remove Favourites</span></i></button>
-												@endif												
+												<p class="left-sub-text">Finger foods including burgers. This bar is sort of perfect.First of all it's right across from the police station...</p>
+												<p class="read">
+													<a href="{{ route('frontend_more_business',['q'=>$business['business_id']]) }}">Read More |</a>
+													<a target="_blank" href="{{ $business['business_website'] }}">Website</a>
+													@if(Auth::check() && Auth::user()->user_id == $business->created_by)
+														<a href="{{ route('edit_business',['q'=> $business['business_id']]) }}">| Edit</a>
+													@endif
+												</p>
 											</div>
-											
+											<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 text-center socialicon">
+											<div class="fav-btn-container">
+												@if(!Favourite::check($business['business_id'], 1))
+													<button type="button" data-id="{{ $business['business_id'] }}" class="btn favourite add_fav_business"><i class="fa fa-heart" aria-hidden="true"><span class="favourite-btn"> Add To Favorites</span></i></button>
+												@else
+													<button type="button" data-id="{{ $business['business_id'] }}" class="btn favourite rvm_fav_business"><i class="fa fa-heart"  aria-hidden="true"><span class="favourite-btn"> Remove Favorites</span></i></button>
+												@endif
+											</div>
 
-											<p class="text-center text-1"><span><i class="fa fa-heart heart-icon" aria-hidden="true"></i> <span class="fav-count">{{ $event['fav_count'] }}</span> FAVORITES</span></p>
+											<p class="text-center text-1"><span><i class="fa fa-heart heart-icon" aria-hidden="true"></i> <span class="fav-count">{{ $business['fav_count'] }}</span> FAVORITES</span></p>
+
 											<div class="icon">
 
-											@if($event['event_fb_link'])
-
-												<a class="btn btn-social-icon btn-facebook facebook" href="{{ $event['event_fb_link'] }}" target="_blank"><span class="fa fa-facebook"></span></a>
-
+											@if($business['business_fb_link'])
+													<a class="btn btn-social-icon btn-facebook facebook" href="{{ $business['business_fb_link'] }}" target="_blank"><span class="fa fa-facebook"></span></a>
 											@endif
 
-												<a class="btn btn-social-icon btn-envelope email" href="mailto:{{ $event['event_email'] }}"><span class="fa fa-envelope"></span></a>
 
-											@if($event['event_twitter_link'])
+													<a class="btn btn-social-icon btn-envelope email" href="mailto:{{ $business['business_email'] }}"><span class="fa fa-envelope"></span></a>
 
-												<a class="btn btn-social-icon btn-twitter twitter" href="{{ $event['event_twitter_link'] }}" target="_blank"><span class="fa fa-twitter"></span></a>
+												@if($business['business_twitter_link'])
 
-											@endif
+													<a class="btn btn-social-icon btn-twitter twitter" href="{{ $business['business_twitter_link'] }}" target="_blank"><span class="fa fa-twitter"></span></a>
 
+												@endif
+
+												</div>
 											</div>
 										</div>
+										@endforeach
+										<div class="col-lg-12 col-md-12 col-xs-12 text-center">
+											
+										</div>
 									</div>
-									@endforeach
-									<div class="col-lg-12 col-md-12 col-xs-12 text-center">
-										
-									</div>
-									<div class="col-lg-12 col-md-12 col-xs-12 text-center">
-										{{ $all_events->links() }}
-									</div>
-								</div>
-							@endif
+								@endif
+							<!--end business div-->
+							<!--start event div-->
+								@if( count($all_events) > 0 )
+									<div class="eventmain businessevent">
+										<h3 class="business-text">Events:</h3>
+										@foreach($all_events as $event)
+										<div class="col-lg-12 col-md-12 col-xs-12 devide">
+											<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 divimgs">
+												<a href="{{ route('frontend_more_event',['q'=>$event['event_id']]) }}">
 
-							@if( count($all_events) == 0 && count($all_business) == 0 )
-								<h4 class="nothing-found">Nothing found</h4>
+													@if(file_exists(public_path().'/'.'images'.'/'.'event/'.$event['image'][0]) == 1)
+														<img src="{{ url('/images/event/'.$event['image'][0]) }}" class="img-responsive thumb-img placeholder">
+													@else
+														<img src="{{ url('/images/placeholder.svg') }}" class="img-responsive thumb-img placeholder">
+
+													@endif
+												</a>
+											</div>
+											<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 textdetails">
+												<h4 class="head"><a href="{{ route('frontend_more_event',['q'=>$event['event_id']]) }}">{{ $event['event_title'] }}</a></h4>
+
+												@if( count($event['tags']) > 0 )
+													<h5 class="colors">Listed in 
+													@foreach($event['tags'] as $value)
+														@php
+															$unserialize_array = unserialize($value['tags_id']);
+														@endphp
+														@foreach($unserialize_array as $tag)
+															<a href="#">{{ TagName::getTagName($tag) }},</a>
+														@endforeach
+													@endforeach
+													</h5>
+												@endif
+
+												<p class="left-sub-text">Finger foods including burgers. This bar is sort of perfect.First of all it's right across from the police station...</p>
+												<p class="read">
+													<a href="{{ route('frontend_more_event',['q'=>$event['event_id']]) }}">Read More </a>
+													<a target="_blank" href="{{ $event['event_website'] }}">| Website</a>
+													@if(Auth::check() && Auth::user()->user_id == $event->created_by)
+														<a href="{{ route('edit_event',['q'=> $event['event_id']]) }}">| Edit</a>
+													@endif
+												</p>
+											</div>
+											<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 text-center socialicon">
+												<div class="fav-btn-container">
+													@if(!Favourite::check($event['event_id'], 2))
+														<button type="button" data-id="{{ $event['event_id'] }}" class="btn favourite add_fav_event"><i class="fa fa-heart" aria-hidden="true"><span class="favourite-btn"> Add To Favourites</span></i></button>
+													@else
+														<button type="button"  data-id="{{ $event['event_id'] }}" class="btn favourite rvm_fav_event"><i class="fa fa-heart" aria-hidden="true"><span class="favourite-btn"> Remove Favourites</span></i></button>
+													@endif												
+												</div>
+												
+
+												<p class="text-center text-1"><span><i class="fa fa-heart heart-icon" aria-hidden="true"></i> <span class="fav-count">{{ $event['fav_count'] }}</span> FAVORITES</span></p>
+												<div class="icon">
+
+												@if($event['event_fb_link'])
+
+													<a class="btn btn-social-icon btn-facebook facebook" href="{{ $event['event_fb_link'] }}" target="_blank"><span class="fa fa-facebook"></span></a>
+
+												@endif
+
+													<a class="btn btn-social-icon btn-envelope email" href="mailto:{{ $event['event_email'] }}"><span class="fa fa-envelope"></span></a>
+
+												@if($event['event_twitter_link'])
+
+													<a class="btn btn-social-icon btn-twitter twitter" href="{{ $event['event_twitter_link'] }}" target="_blank"><span class="fa fa-twitter"></span></a>
+
+												@endif
+
+												</div>
+											</div>
+										</div>
+										@endforeach
+										<div class="col-lg-12 col-md-12 col-xs-12 text-center">
+											
+										</div>
+										<div class="col-lg-12 col-md-12 col-xs-12 text-center">
+											{{ $all_events->links() }}
+										</div>
+									</div>
+								@endif
+
+								@if( count($all_events) == 0 && count($all_business) == 0 )
+									<h4 class="nothing-found">Nothing found</h4>
+								@endif
 							@endif
 						</div>
 					</div>
@@ -280,6 +362,15 @@
 	$(".datecalender").on("dp.hide", function (e) {
         $(this).parent().removeClass('dates');
     });
-
+    $(document).ready(function(){
+    	$('#radio1').click(function(){
+    		$('#fromDateDiv').hide();
+    		$('#toDateDiv').hide();
+    	});
+    	$('#radio2').click(function(){
+    		$('#fromDateDiv').show();
+    		$('#toDateDiv').show();
+    	});
+    });
 </script>
 @endsection
