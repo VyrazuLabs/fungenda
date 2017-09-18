535b4dc204a3666dfc8784113d44a53e3f9718d7
Anand <anand.singh989@gmail.com>
2017-09-18 18:05:39 +0530

    bug fixing

--- laravel/resources/views/admin/business/create-business.blade.php
+++ laravel/resources/views/admin/business/create-business.blade.php
@@ -69,7 +69,6 @@
                         </div>
                         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 form-group profilegroup createeventgroup createeventadmin-div">
                             {{Form::label('image', 'Image')}}
-                            <span class="require-star"></span>
                             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 eventimagediv"> 
                               <div class="col-lg-10 col-md-10 col-sm-9 col-xs-12 eventtextboxdiv">
                                 <div id="businessupload" class="upload-file-container" >
