0d362297f72ea0af147fc042176a6af61ee1a813
Anand <anand.singh989@gmail.com>
2017-10-04 17:01:28 +0530

    working on mail functionality

---
+++ laravel/resources/views/email/remove_from_favourite_shared_location_email.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<center>
+	<h1>You have removed a shared location from your favorite list successfully</h1>
+</center>
+</body>
+</html>
