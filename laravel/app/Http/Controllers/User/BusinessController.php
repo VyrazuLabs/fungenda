9309eec1699305601ed7c4f3f98d58081fd2de9c
Anand <anand.singh989@gmail.com>
2017-10-20 12:06:20 +0530

    added add to favorite business email template

--- laravel/app/Http/Controllers/User/BusinessController.php
+++ laravel/app/Http/Controllers/User/BusinessController.php
@@ -613,6 +613,8 @@ class BusinessController extends Controller
                         'status' => 1,
                     ]);
 
+                $data = Business::where('business_id',$input['business_id'])->first();
+
                 $all_fav_data = MyFavorite::where('entity_type',1)->where('entity_id',$input['business_id'])->get();
 
                 $count = count($all_fav_data);
@@ -620,7 +622,7 @@ class BusinessController extends Controller
                 $email = Auth::user()->email;
                 $first_name = Auth::user()->first_name;
 
-                Mail::send('email.business_email',['name' => 'Efungenda'],function($message) use($email,$first_name){
+                Mail::send('email.business_email',['name' => 'Efungenda','first_name'=>$first_name,'data'=>$data],function($message) use($email,$first_name){
                   $message->from('vyrazulabs@gmail.com', $name = null)->to($email,$first_name)->subject('Add to favorite Successfull');
                 });
 
