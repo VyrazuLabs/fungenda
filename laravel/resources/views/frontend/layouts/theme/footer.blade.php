25606ea66c6f15b92eb444615ab0a2e8f0534543
vyrazu-0206 <anand.singh989@gmail.com>
2018-01-16 20:09:49 +0530

    bug fixing

--- laravel/resources/views/frontend/layouts/theme/footer.blade.php
+++ laravel/resources/views/frontend/layouts/theme/footer.blade.php
@@ -267,6 +267,7 @@ function googleTranslateElementInit() {
 </script>
 <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
 
+
 <script type="text/javascript">
     /***************************
           PNOTIFY GLOBAL POPUPS
